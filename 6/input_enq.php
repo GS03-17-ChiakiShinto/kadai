baefa8f850a37a0225540add7348df4cdd798808
aaa <aaa@example.com>
2016-01-16 14:04:29 +0900

    Revert "アンケート回答フォーム（不要な行を削除）"
    This reverts commit 74c33660f86562f1ed1296a1d017794482bca9ed.

--- 6/input_enq.php
+++ 6/input_enq.php
@@ -42,11 +42,13 @@
 		<p>
 
 		</p>
-		<p>	
+		<p>
+		<!--あとで選択個数制限+1つ以上は入力させる制御入れる-->	
 			趣味（3つまで）：
 		</p>
 		<!--チェックボックスの value の値は$_POST[hobby][0], $_POST[hobby][1],…に格納される。hobbyはname属性部分-->
-		<!--一度別配列に格納し、別途account配列に追加する-->
+		<!--本当は上記のaccount配列に追加していきたいけど難しそう-->
+		<!--一度別配列に入れてあとからaccount配列に追加する？-->
 		<input type="checkbox" name="hobby[]" value="読書">読書
 		<input type="checkbox" name="hobby[]" value="スポーツ">スポーツ
 		<input type="checkbox" name="hobby[]" value="映画">映画
